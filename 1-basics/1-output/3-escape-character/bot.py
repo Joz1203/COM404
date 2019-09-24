d5fc773b1a4b93a2bbce493b581f734e682e8a19
Joz1203 <joannadavie1991@gmail.com>
2019-09-24 15:10:58 +0000

    Added code to display escape characters

---
+++ 1-basics/1-output/3-escape-character/bot.py
@@ -0,0 +1,15 @@
+# Display message to the standard output
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System online.")
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
