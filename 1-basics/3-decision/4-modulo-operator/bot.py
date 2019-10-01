f8bb98975d18c113ec31a9a12ed4eb743829235a
Joz1203 <joannadavie1991@gmail.com>
2019-10-01 10:23:35 +0000

    Added code to demonstrate the use of a modulus operator.

---
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,6 @@
+print("Please enter a whole number.")
+number = int(input())
+if (number % 2 == 0):
+    print("The number " + str(number) + " is even.") 
+elif (number % 2 == 1):
+    print("The number " + str(number) + " is odd.") 
