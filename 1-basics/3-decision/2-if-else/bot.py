349ee1875ccfb4f06fd76b6ae74942f7fe0ae9b5
Joz1203 <joannadavie1991@gmail.com>
2019-10-01 09:25:14 +0000

    Added code to demonstrate the use of an if...else statement.

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,7 @@
+print ("Please enter the activity to be performed.")
+activity = input() 
+if activity == "calculate":
+    print("Performing calculations...")
+elif activity != "calculate":
+    print("Performing activity...")
+print("Activity completed!")
