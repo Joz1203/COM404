b3b2309d2797a4c1af1bec127cabe73c4ad2023e
Joz1203 <joannadavie1991@gmail.com>
2019-10-14 20:03:37 +0000

    edited

--- 1-basics/4-repetition/1-while-loop/2-count/bot.py
+++ 1-basics/4-repetition/1-while-loop/2-count/bot.py
@@ -2,6 +2,7 @@ print("How many live cables should I avoid?")
 amount = int(input(""))
 count = 0
 while count in amount:  
-    count = count + 1
     print("Avoiding...Done!" , count, "live cables avoided")
+    count = count + 1
+    
 print("All live cables have been avoided.")
