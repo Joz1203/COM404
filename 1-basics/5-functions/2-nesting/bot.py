ea61528ca76a8de849129577ef38c16d1065bcb6
Joz1203 <joannadavie1991@gmail.com>
2019-10-15 16:25:53 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ 1-basics/5-functions/2-nesting/bot.py
@@ -0,0 +1,12 @@
+# Identify 
+def indentify ():
+    print("What lies before us?")
+    thing = input()
+    print()
+    if thing == "a large boulder":
+        print("It's time to run!")
+    else: 
+        print("We will be fine.")
+
+# Call Identify
+indentify ()
